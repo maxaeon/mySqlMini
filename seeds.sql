0e8972ab4607dfe0234dbb61f3b7176a00a299a4
Aldo <aivhb215@gmail.com>
2021-10-13 21:18:26 -0400

    added seeds file

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (movie_name)
+VALUES ("Snowpiercer");
+        ("Candyman");
+        ("Free Guy");
+        ("Knives Out");
+        ("The Matrix");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "best movie ever");
+        (2, "Way too creepy");
+        (3, "10/10 Would watch again");
+        (4, "Very Entertaining");
+        (5, "Second best movie ever");
