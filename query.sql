9fdf51b3a705f3594101d38af1b51a3287cea65d
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2021-10-13 20:53:44 -0400

    query section

---
+++ query.sql
@@ -0,0 +1,8 @@
+-- created movies.movie_name
+-- created reviews.review
+
+SELECT movies.movie_name AS movie, reviews.review
+FROM reviews
+LEFT JOIN movies
+ON reviews.movie_id = movies.id
+ORDER BY movies.movie_name;
